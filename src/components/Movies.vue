007edd0c86381507403bb6a533a2c9f13b71e661
Gabriel Henrique <bielriquecustodio@gmail.com>
2022-10-19 12:10:25 -0300

    Created the carousel that will present the list of movies.

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="container-list-movies">
-    <p class="group-movies">Top 20 hoje</p>
+    <p class="group-movies">Top 20 melhores avaliados</p>
     <div class="container-boxes">
-      <div id="box" class="box" v-for="m in movies" :key="m.id">
-        <img :src="`${image}${m.backdrop_path}`" alt="" :title="m.title" />
+      <div class="box">
+        <img
+          v-for="m in movies"
+          :key="m.id"
+          :src="`${image}${m.backdrop_path}`"
+          alt=""
+          :title="m.title"
+        />
       </div>
-      <i class="fa-solid fa-chevron-right" @click="next"></i>
+      <i
+        id="switchLeft"
+        class="fa-solid fa-chevron-left"
+        @click="showMovieLeft"
+      ></i>
+      <i
+        id="switchRight"
+        class="fa-solid fa-chevron-right"
+        @click="showMovieRight"
+      ></i>
     </div>
   </div>
 </template>
@@ -18,7 +33,8 @@ export default {
     return {
       movies: "",
       image: config.imageTmdb,
-      box: 0
+      scrollPerClick: 275,
+      scrollAmount: 0,
     };
   },
   created() {
@@ -31,10 +47,33 @@ export default {
       });
   },
   methods: {
-    next() {
-      console.log('Clique reconhecido')
-    }
-  }
+    showMovieLeft() {
+      const movies = document.querySelector(".box");
+
+      movies.scrollTo({
+        top: 0,
+        left: (this.scrollAmount -= this.scrollPerClick),
+        behavior: "smooth",
+      });
+
+      if (this.scrollAmount < 0) {
+        this.scrollAmount = 0;
+      }
+    },
+    showMovieRight() {
+      const movies = document.querySelector(".box");
+
+      movies.scrollTo({
+        top: 0,
+        left: (this.scrollAmount += this.scrollPerClick),
+        behavior: "smooth",
+      });
+
+      if (this.scrollAmount > 0) {
+        document.getElementById('switchLeft').style.display = 'flex'
+      }
+    },
+  },
 };
 </script>
 
@@ -62,45 +101,77 @@ export default {
 }
 
 .container-boxes {
-  width: 100%;
-  overflow: hidden;
-  display: flex;
-  padding-left: 50px;
+  height: 180px;
+  width: 93%;
   position: relative;
-}
-
-.box {
   display: flex;
-  transition: transform 0.5 ease-in-out;
-  transform: translateX(0);
-  cursor: pointer;
 }
 
-.box img {
+.container-boxes .box {
+  width: auto;
+  overflow: scroll;
+  text-align: center;
+  padding-bottom: 10px !important;
+  display: flex;
+  align-items: center;
+}
+
+.container-boxes .box::-webkit-scrollbar {
+  display: none;
+}
+
+.container-boxes .box img {
+  min-width: 260px;
+  max-width: 260px;
+  height: auto;
+  background-size: cover;
+  margin-left: 15px;
+  cursor: pointer;
+  transition: 0.5s ease-in-out;
+  z-index: 2;
   object-fit: cover;
   border: none;
   border-radius: 20px;
-  width: 260px;
-  margin-left: 8px;
 }
 
-.container-boxes i {
-  height: 100%;
-  position: absolute;
+.container-boxes .box img:first-child {
+  margin-left: 0;
+}
+
+.container-boxes .box img:hover {
+  transform: scale(1.1);
+  z-index: 5;
+}
+
+.container-boxes #switchLeft,
+.container-boxes #switchRight {
   display: flex;
   align-items: center;
   justify-content: center;
+  height: 100%;
+  width: 3%;
   color: #fff;
   font-size: 1.7vw;
-  z-index: 10;
-  width: 40px;
-  margin-left: calc(100% - 110px);
+  background: blue;
+  top: 0;
   opacity: 0;
+  z-index: 3;
 }
 
-.container-boxes i:hover {
+.container-boxes #switchLeft {
+  position: absolute;
+  left: -50px;
+  display: none;
+}
+
+.container-boxes #switchRight {
+  position: absolute;
+  right: -50px;
+}
+
+.container-boxes #switchLeft:hover,
+.container-boxes #switchRight:hover {
   cursor: pointer;
-  width: 40px;
   opacity: 1;
   background: rgba(33, 33, 48, 0.4);
 }
